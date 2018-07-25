e2fa52bb5491fe78318406719e3f2162bba8595c
sniperwang00@gmail.com <wangH1988>
2018-07-25 23:57:42 +0800

    commit second time

--- src/main/java/com/luke/anything/GitBranchCommit.java
+++ src/main/java/com/luke/anything/GitBranchCommit.java
@@ -4,4 +4,8 @@ public class GitBranchCommit {
 	public void output(){
 		System.out.println("this is the first commit!");
 	}
+
+	public void commit2(){
+		System.out.println("this is the second commit!");
+	}
 }
